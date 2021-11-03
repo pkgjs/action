cec70ccef8d4e7614243e3379a7bb71a956b098c
Dominykas Blyžė <hello@dominykas.com>
2021-11-03 18:46:05 +0200

    feat: prepare-dynamic-steps action

---
+++ .github/actions/prepare-dynamic-steps/index.js
@@ -0,0 +1,51 @@
+'use strict';
+
+process.on('unhandledRejection', (err) => {
+
+    throw err;
+});
+
+
+const ActionsCore = require('@actions/core');
+const Fs = require('fs');
+const Path = require('path');
+const Yaml = require('yaml');
+
+
+exports.main = function () {
+
+    const outputFolder = Path.join('.github', 'tmp', ActionsCore.getInput('path'));
+
+    Fs.mkdirSync(outputFolder, { recursive: true });
+
+    try {
+        var steps = Yaml.parse(ActionsCore.getInput('steps'));
+    }
+    catch (err) {
+        throw new Error('Invalid `steps` - unable to parse YAML.');
+    }
+
+    if (!Array.isArray(steps)) {
+        throw new Error('Invalid `steps` - not an array.');
+    }
+
+    const action = {
+        runs: {
+            using: 'composite',
+            steps
+        }
+    };
+
+    const actionYaml = Yaml.stringify(action);
+
+    const outputPath = Path.join(outputFolder, 'action.yaml');
+
+    console.log(`Writing ${outputPath}...`);
+    console.log(actionYaml);
+    Fs.writeFileSync(outputPath, actionYaml);
+};
+
+
+if (require.main === module) {
+    exports.main();
+}
---
+++ .github/actions/prepare-dynamic-steps/test/index.js
@@ -0,0 +1,53 @@
+'use strict';
+
+process.on('unhandledRejection', (err) => {
+
+    throw err;
+});
+
+
+const Assert = require('assert');
+const Fs = require('fs');
+const Path = require('path');
+const PrepareDynamicStepsAction = require('..');
+
+
+const expectedResult1 = `
+runs:
+  using: composite
+  steps:
+    - run: echo ohai
+    - run: echo ohai again
+`;
+
+
+exports.main = function () {
+
+    const originalEnv = { ...process.env };
+
+    const tmpFolder = Path.join(__dirname, '.tmp', `${Date.now()}`);
+
+    try {
+
+        Fs.mkdirSync(tmpFolder, { recursive: true });
+        process.chdir(tmpFolder);
+
+        process.env = { ...originalEnv, INPUT_PATH: 'test1', INPUT_STEPS: '- run: echo ohai\n- run: echo ohai again' };
+        PrepareDynamicStepsAction.main();
+        Assert.strictEqual(Fs.readFileSync(Path.join(tmpFolder, '.github', 'tmp', 'test1', 'action.yaml')).toString().trim(), expectedResult1.trim());
+
+        process.env = { ...originalEnv, INPUT_PATH: 'test2', INPUT_STEPS: '- item\n-invalid' };
+        Assert.throws(() => PrepareDynamicStepsAction.main(), { message: 'Invalid `steps` - unable to parse YAML.' });
+
+        process.env = { ...originalEnv, INPUT_PATH: 'test3', INPUT_STEPS: 'not an array' };
+        Assert.throws(() => PrepareDynamicStepsAction.main(), { message: 'Invalid `steps` - not an array.' });
+    }
+    finally {
+        Fs.rmdirSync(Path.join(__dirname, '.tmp'), { recursive: true });
+    }
+};
+
+
+if (require.main === module) {
+    exports.main();
+}
