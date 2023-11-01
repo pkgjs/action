f707e475c5b58295773852c4ebd651d9cd100644
Dominykas Blyžė <hello@dominykas.com>
2023-11-01 22:17:26 +0200

    test: change assertions to remove the github ::set-output prefix
    This will make it simpler to refactor the tests to rely on GITHUB_OUTPUT variable again, instead of hijacking the stdout.
    
    (cherry picked from commit ebed8dea2ec84154b8dd3a8be09ba89ac45c6231)

--- .github/actions/prepare-node-test-matrix-action/test/index.js
+++ .github/actions/prepare-node-test-matrix-action/test/index.js
@@ -17,7 +17,8 @@ exports.getOutput = function (now, pkg) {
 
         line = line.trim();
         if (line) {
-            output.push(line);
+            Assert.equal(line.slice(0, 13), '::set-output ');
+            output.push(line.slice(13));
         }
     };
 
@@ -68,32 +69,32 @@ describe('PrepareNodeTextMatrixAction', () => {
             ...originalEnv
         };
         Assert.deepStrictEqual(exports.getOutput(new Date('2020-07-01'), { engines: { node: '^14' } }), [
-            '::set-output name=node-version::[]',
-            '::set-output name=lts-latest::12',
-            '::set-output name=runs-on::[null]',
-            '::set-output name=include::[{"node-version":14,"runs-on":null,"experimental":"experimental"}]',
-            '::set-output name=exclude::[]'
+            'name=node-version::[]',
+            'name=lts-latest::12',
+            'name=runs-on::[null]',
+            'name=include::[{"node-version":14,"runs-on":null,"experimental":"experimental"}]',
+            'name=exclude::[]'
         ]);
         Assert.deepStrictEqual(exports.getOutput(new Date('2020-07-01'), { engines: { node: '*' } }), [
-            '::set-output name=node-version::[12,10,8,6,4]',
-            '::set-output name=lts-latest::12',
-            '::set-output name=runs-on::[null]',
-            '::set-output name=include::[{"node-version":14,"runs-on":null,"experimental":"experimental"}]',
-            '::set-output name=exclude::[]'
+            'name=node-version::[12,10,8,6,4]',
+            'name=lts-latest::12',
+            'name=runs-on::[null]',
+            'name=include::[{"node-version":14,"runs-on":null,"experimental":"experimental"}]',
+            'name=exclude::[]'
         ]);
         Assert.deepStrictEqual(exports.getOutput(new Date('2020-07-01'), { engines: { node: '^14 || ^12 || ^10' } }), [
-            '::set-output name=node-version::[12,10]',
-            '::set-output name=lts-latest::12',
-            '::set-output name=runs-on::[null]',
-            '::set-output name=include::[{"node-version":14,"runs-on":null,"experimental":"experimental"}]',
-            '::set-output name=exclude::[]'
+            'name=node-version::[12,10]',
+            'name=lts-latest::12',
+            'name=runs-on::[null]',
+            'name=include::[{"node-version":14,"runs-on":null,"experimental":"experimental"}]',
+            'name=exclude::[]'
         ]);
         Assert.deepStrictEqual(exports.getOutput(new Date('2010-07-01'), { engines: { node: '*' } }), [
-            '::set-output name=node-version::[]',
-            '::set-output name=lts-latest::4',
-            '::set-output name=runs-on::[null]',
-            '::set-output name=include::[]',
-            '::set-output name=exclude::[]'
+            'name=node-version::[]',
+            'name=lts-latest::4',
+            'name=runs-on::[null]',
+            'name=include::[]',
+            'name=exclude::[]'
         ]);
     });
 
@@ -104,39 +105,39 @@ describe('PrepareNodeTextMatrixAction', () => {
             'INPUT_UPGRADE-POLICY': 'lts'
         };
         Assert.deepStrictEqual(exports.getOutput(new Date('2020-07-01'), { engines: { node: '^10' } }), [
-            '::set-output name=node-version::[12,10]',
-            '::set-output name=lts-latest::12',
-            '::set-output name=runs-on::[null]',
-            '::set-output name=include::[{"node-version":14,"runs-on":null,"experimental":"experimental"}]',
-            '::set-output name=exclude::[]'
+            'name=node-version::[12,10]',
+            'name=lts-latest::12',
+            'name=runs-on::[null]',
+            'name=include::[{"node-version":14,"runs-on":null,"experimental":"experimental"}]',
+            'name=exclude::[]'
         ]);
         Assert.deepStrictEqual(exports.getOutput(new Date('2020-11-01'), { engines: { node: '^10' } }), [
-            '::set-output name=node-version::[14,12,10]',
-            '::set-output name=lts-latest::14',
-            '::set-output name=runs-on::[null]',
-            '::set-output name=include::[{"node-version":15,"runs-on":null,"experimental":"experimental"}]',
-            '::set-output name=exclude::[]'
+            'name=node-version::[14,12,10]',
+            'name=lts-latest::14',
+            'name=runs-on::[null]',
+            'name=include::[{"node-version":15,"runs-on":null,"experimental":"experimental"}]',
+            'name=exclude::[]'
         ]);
         Assert.deepStrictEqual(exports.getOutput(new Date('2021-05-01'), { engines: { node: '^10' } }), [
-            '::set-output name=node-version::[14,12,10]',
-            '::set-output name=lts-latest::14',
-            '::set-output name=runs-on::[null]',
-            '::set-output name=include::[{"node-version":16,"runs-on":null,"experimental":"experimental"},{"node-version":15,"runs-on":null,"experimental":"experimental"}]',
-            '::set-output name=exclude::[]'
+            'name=node-version::[14,12,10]',
+            'name=lts-latest::14',
+            'name=runs-on::[null]',
+            'name=include::[{"node-version":16,"runs-on":null,"experimental":"experimental"},{"node-version":15,"runs-on":null,"experimental":"experimental"}]',
+            'name=exclude::[]'
         ]);
         Assert.deepStrictEqual(exports.getOutput(new Date('2021-07-01'), { engines: { node: '^10' } }), [
-            '::set-output name=node-version::[14,12,10]',
-            '::set-output name=lts-latest::14',
-            '::set-output name=runs-on::[null]',
-            '::set-output name=include::[{"node-version":16,"runs-on":null,"experimental":"experimental"}]',
-            '::set-output name=exclude::[]'
+            'name=node-version::[14,12,10]',
+            'name=lts-latest::14',
+            'name=runs-on::[null]',
+            'name=include::[{"node-version":16,"runs-on":null,"experimental":"experimental"}]',
+            'name=exclude::[]'
         ]);
         Assert.deepStrictEqual(exports.getOutput(new Date('2021-11-01'), { engines: { node: '^10' } }), [
-            '::set-output name=node-version::[16,14,12,10]',
-            '::set-output name=lts-latest::16',
-            '::set-output name=runs-on::[null]',
-            '::set-output name=include::[{"node-version":17,"runs-on":null,"experimental":"experimental"}]',
-            '::set-output name=exclude::[]'
+            'name=node-version::[16,14,12,10]',
+            'name=lts-latest::16',
+            'name=runs-on::[null]',
+            'name=include::[{"node-version":17,"runs-on":null,"experimental":"experimental"}]',
+            'name=exclude::[]'
         ]);
     });
 
@@ -147,39 +148,39 @@ describe('PrepareNodeTextMatrixAction', () => {
             'INPUT_UPGRADE-POLICY': 'lts/strict'
         };
         Assert.deepStrictEqual(exports.getOutput(new Date('2020-07-01'), { engines: { node: '^10' } }), [
-            '::set-output name=node-version::[12,10]',
-            '::set-output name=lts-latest::12',
-            '::set-output name=runs-on::[null]',
-            '::set-output name=include::[]',
-            '::set-output name=exclude::[]'
+            'name=node-version::[12,10]',
+            'name=lts-latest::12',
+            'name=runs-on::[null]',
+            'name=include::[]',
+            'name=exclude::[]'
         ]);
         Assert.deepStrictEqual(exports.getOutput(new Date('2020-11-01'), { engines: { node: '^10' } }), [
-            '::set-output name=node-version::[14,12,10]',
-            '::set-output name=lts-latest::14',
-            '::set-output name=runs-on::[null]',
-            '::set-output name=include::[]',
-            '::set-output name=exclude::[]'
+            'name=node-version::[14,12,10]',
+            'name=lts-latest::14',
+            'name=runs-on::[null]',
+            'name=include::[]',
+            'name=exclude::[]'
         ]);
         Assert.deepStrictEqual(exports.getOutput(new Date('2021-05-01'), { engines: { node: '^10' } }), [
-            '::set-output name=node-version::[14,12,10]',
-            '::set-output name=lts-latest::14',
-            '::set-output name=runs-on::[null]',
-            '::set-output name=include::[]',
-            '::set-output name=exclude::[]'
+            'name=node-version::[14,12,10]',
+            'name=lts-latest::14',
+            'name=runs-on::[null]',
+            'name=include::[]',
+            'name=exclude::[]'
         ]);
         Assert.deepStrictEqual(exports.getOutput(new Date('2021-07-01'), { engines: { node: '^10' } }), [
-            '::set-output name=node-version::[14,12,10]',
-            '::set-output name=lts-latest::14',
-            '::set-output name=runs-on::[null]',
-            '::set-output name=include::[]',
-            '::set-output name=exclude::[]'
+            'name=node-version::[14,12,10]',
+            'name=lts-latest::14',
+            'name=runs-on::[null]',
+            'name=include::[]',
+            'name=exclude::[]'
         ]);
         Assert.deepStrictEqual(exports.getOutput(new Date('2021-11-01'), { engines: { node: '^10' } }), [
-            '::set-output name=node-version::[16,14,12,10]',
-            '::set-output name=lts-latest::16',
-            '::set-output name=runs-on::[null]',
-            '::set-output name=include::[]',
-            '::set-output name=exclude::[]'
+            'name=node-version::[16,14,12,10]',
+            'name=lts-latest::16',
+            'name=runs-on::[null]',
+            'name=include::[]',
+            'name=exclude::[]'
         ]);
     });
 
@@ -190,39 +191,39 @@ describe('PrepareNodeTextMatrixAction', () => {
             'INPUT_UPGRADE-POLICY': 'all'
         };
         Assert.deepStrictEqual(exports.getOutput(new Date('2020-07-01'), { engines: { node: '^10' } }), [
-            '::set-output name=node-version::[14,13,12,11,10]',
-            '::set-output name=lts-latest::12',
-            '::set-output name=runs-on::[null]',
-            '::set-output name=include::[]',
-            '::set-output name=exclude::[]'
+            'name=node-version::[14,13,12,11,10]',
+            'name=lts-latest::12',
+            'name=runs-on::[null]',
+            'name=include::[]',
+            'name=exclude::[]'
         ]);
         Assert.deepStrictEqual(exports.getOutput(new Date('2020-11-01'), { engines: { node: '^10' } }), [
-            '::set-output name=node-version::[15,14,13,12,11,10]',
-            '::set-output name=lts-latest::14',
-            '::set-output name=runs-on::[null]',
-            '::set-output name=include::[]',
-            '::set-output name=exclude::[]'
+            'name=node-version::[15,14,13,12,11,10]',
+            'name=lts-latest::14',
+            'name=runs-on::[null]',
+            'name=include::[]',
+            'name=exclude::[]'
         ]);
         Assert.deepStrictEqual(exports.getOutput(new Date('2021-05-01'), { engines: { node: '^10' } }), [
-            '::set-output name=node-version::[16,15,14,13,12,11,10]',
-            '::set-output name=lts-latest::14',
-            '::set-output name=runs-on::[null]',
-            '::set-output name=include::[]',
-            '::set-output name=exclude::[]'
+            'name=node-version::[16,15,14,13,12,11,10]',
+            'name=lts-latest::14',
+            'name=runs-on::[null]',
+            'name=include::[]',
+            'name=exclude::[]'
         ]);
         Assert.deepStrictEqual(exports.getOutput(new Date('2021-07-01'), { engines: { node: '^10' } }), [
-            '::set-output name=node-version::[16,15,14,13,12,11,10]',
-            '::set-output name=lts-latest::14',
-            '::set-output name=runs-on::[null]',
-            '::set-output name=include::[]',
-            '::set-output name=exclude::[]'
+            'name=node-version::[16,15,14,13,12,11,10]',
+            'name=lts-latest::14',
+            'name=runs-on::[null]',
+            'name=include::[]',
+            'name=exclude::[]'
         ]);
         Assert.deepStrictEqual(exports.getOutput(new Date('2021-11-01'), { engines: { node: '^10' } }), [
-            '::set-output name=node-version::[17,16,15,14,13,12,11,10]',
-            '::set-output name=lts-latest::16',
-            '::set-output name=runs-on::[null]',
-            '::set-output name=include::[]',
-            '::set-output name=exclude::[]'
+            'name=node-version::[17,16,15,14,13,12,11,10]',
+            'name=lts-latest::16',
+            'name=runs-on::[null]',
+            'name=include::[]',
+            'name=exclude::[]'
         ]);
     });
 
@@ -233,11 +234,11 @@ describe('PrepareNodeTextMatrixAction', () => {
             'INPUT_RUNS-ON': 'ubuntu-LATEST'
         };
         Assert.deepStrictEqual(exports.getOutput(new Date('2020-07-01'), { engines: { node: '^14' } }), [
-            '::set-output name=node-version::[]',
-            '::set-output name=lts-latest::12',
-            '::set-output name=runs-on::[null]',
-            '::set-output name=include::[{"node-version":14,"runs-on":null,"experimental":"experimental"}]',
-            '::set-output name=exclude::[]'
+            'name=node-version::[]',
+            'name=lts-latest::12',
+            'name=runs-on::[null]',
+            'name=include::[{"node-version":14,"runs-on":null,"experimental":"experimental"}]',
+            'name=exclude::[]'
         ]);
     });
 
@@ -248,11 +249,11 @@ describe('PrepareNodeTextMatrixAction', () => {
             'INPUT_RUNS-ON': 'ubuntu-latest, windows-latest, macos-latest'
         };
         Assert.deepStrictEqual(exports.getOutput(new Date('2020-07-01'), { engines: { node: '^14' } }), [
-            '::set-output name=node-version::[]',
-            '::set-output name=lts-latest::12',
-            '::set-output name=runs-on::["ubuntu-latest","windows-latest","macos-latest"]',
-            '::set-output name=include::[{"node-version":14,"runs-on":"ubuntu-latest","experimental":"experimental"},{"node-version":14,"runs-on":"windows-latest","experimental":"experimental"},{"node-version":14,"runs-on":"macos-latest","experimental":"experimental"}]',
-            '::set-output name=exclude::[]'
+            'name=node-version::[]',
+            'name=lts-latest::12',
+            'name=runs-on::["ubuntu-latest","windows-latest","macos-latest"]',
+            'name=include::[{"node-version":14,"runs-on":"ubuntu-latest","experimental":"experimental"},{"node-version":14,"runs-on":"windows-latest","experimental":"experimental"},{"node-version":14,"runs-on":"macos-latest","experimental":"experimental"}]',
+            'name=exclude::[]'
         ]);
     });
 
@@ -263,11 +264,11 @@ describe('PrepareNodeTextMatrixAction', () => {
             'INPUT_RUNS-ON': '- ubuntu-latest\n- windows-latest\n- macos-latest\n'
         };
         Assert.deepStrictEqual(exports.getOutput(new Date('2020-07-01'), { engines: { node: '^14' } }), [
-            '::set-output name=node-version::[]',
-            '::set-output name=lts-latest::12',
-            '::set-output name=runs-on::["ubuntu-latest","windows-latest","macos-latest"]',
-            '::set-output name=include::[{"node-version":14,"runs-on":"ubuntu-latest","experimental":"experimental"},{"node-version":14,"runs-on":"windows-latest","experimental":"experimental"},{"node-version":14,"runs-on":"macos-latest","experimental":"experimental"}]',
-            '::set-output name=exclude::[]'
+            'name=node-version::[]',
+            'name=lts-latest::12',
+            'name=runs-on::["ubuntu-latest","windows-latest","macos-latest"]',
+            'name=include::[{"node-version":14,"runs-on":"ubuntu-latest","experimental":"experimental"},{"node-version":14,"runs-on":"windows-latest","experimental":"experimental"},{"node-version":14,"runs-on":"macos-latest","experimental":"experimental"}]',
+            'name=exclude::[]'
         ]);
     });
 
@@ -279,11 +280,11 @@ describe('PrepareNodeTextMatrixAction', () => {
             'INPUT_INCLUDE': '- node-version: 15\n  runs-on: ubuntu-latest\n- node-version: 13'
         };
         Assert.deepStrictEqual(exports.getOutput(new Date('2020-07-01'), { engines: { node: '^14' } }), [
-            '::set-output name=node-version::[]',
-            '::set-output name=lts-latest::12',
-            '::set-output name=runs-on::["ubuntu-latest","windows-latest","macos-latest"]',
-            '::set-output name=include::[{"node-version":14,"runs-on":"ubuntu-latest","experimental":"experimental"},{"node-version":14,"runs-on":"windows-latest","experimental":"experimental"},{"node-version":14,"runs-on":"macos-latest","experimental":"experimental"},{"node-version":15,"runs-on":"ubuntu-latest","experimental":null},{"node-version":13,"runs-on":"ubuntu-latest","experimental":null},{"node-version":13,"runs-on":"windows-latest","experimental":null},{"node-version":13,"runs-on":"macos-latest","experimental":null}]',
-            '::set-output name=exclude::[]'
+            'name=node-version::[]',
+            'name=lts-latest::12',
+            'name=runs-on::["ubuntu-latest","windows-latest","macos-latest"]',
+            'name=include::[{"node-version":14,"runs-on":"ubuntu-latest","experimental":"experimental"},{"node-version":14,"runs-on":"windows-latest","experimental":"experimental"},{"node-version":14,"runs-on":"macos-latest","experimental":"experimental"},{"node-version":15,"runs-on":"ubuntu-latest","experimental":null},{"node-version":13,"runs-on":"ubuntu-latest","experimental":null},{"node-version":13,"runs-on":"windows-latest","experimental":null},{"node-version":13,"runs-on":"macos-latest","experimental":null}]',
+            'name=exclude::[]'
         ]);
     });
 
@@ -295,12 +296,12 @@ describe('PrepareNodeTextMatrixAction', () => {
             'INPUT_EXCLUDE': '- node-version: 17\n  runs-on: ubuntu-latest'
         };
         Assert.deepStrictEqual(exports.getOutput(new Date('2021-11-01'), { engines: { node: '^10' } }), [
-            '::set-output name=node-version::[16,14,12,10]',
-            '::set-output name=lts-latest::16',
-            '::set-output name=runs-on::["ubuntu-latest","windows-latest","macos-latest"]',
+            'name=node-version::[16,14,12,10]',
+            'name=lts-latest::16',
+            'name=runs-on::["ubuntu-latest","windows-latest","macos-latest"]',
             // node 17 on ubuntu latest explicitly excluded
-            '::set-output name=include::[{"node-version":17,"runs-on":"windows-latest","experimental":"experimental"},{"node-version":17,"runs-on":"macos-latest","experimental":"experimental"}]',
-            '::set-output name=exclude::[{"node-version":17,"runs-on":"ubuntu-latest"}]'
+            'name=include::[{"node-version":17,"runs-on":"windows-latest","experimental":"experimental"},{"node-version":17,"runs-on":"macos-latest","experimental":"experimental"}]',
+            'name=exclude::[{"node-version":17,"runs-on":"ubuntu-latest"}]'
         ]);
     });
 });
